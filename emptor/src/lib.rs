7f8b39e0f7c312603f9e82007124c7bba4cb4150
adoerr <0xad@gmx.net>
2021-07-24 06:24:12 +0200

    test prunning leaves

--- emptor/src/lib.rs
+++ emptor/src/lib.rs
@@ -107,30 +107,71 @@ mod tests {
     use super::{insert_header, Block};
 
     use sc_client_api::Backend;
-    use sp_blockchain::HeaderBackend;
+    use sp_blockchain::{Backend as ChainBackend, HeaderBackend};
+    use sp_runtime::generic::BlockId;
 
     #[test]
-    fn insert_header_works() {
-        let db = sc_client_db::Backend::<Block>::new_test(1, 0);
+    fn insert_headers() {
+        let backend = sc_client_db::Backend::<Block>::new_test(1, 0);
 
         for i in 0..10 {
-            assert!(db.blockchain().hash(i).unwrap().is_none());
+            assert!(backend.blockchain().hash(i).unwrap().is_none());
 
             insert_header(
-                &db,
+                &backend,
                 i,
                 if i == 0 {
                     Default::default()
                 } else {
-                    db.blockchain().hash(i - 1).unwrap().unwrap()
+                    backend.blockchain().hash(i - 1).unwrap().unwrap()
                 },
                 None,
                 Default::default(),
             );
 
-            assert!(db.blockchain().hash(i).unwrap().is_some());
+            assert!(backend.blockchain().hash(i).unwrap().is_some());
         }
 
-        assert_eq!(9, db.blockchain().info().best_number);
+        assert_eq!(9, backend.blockchain().info().best_number);
+    }
+
+    #[test]
+    fn prunning_leaves() {
+        let backend = sc_client_db::Backend::<Block>::new_test(10, 10);
+
+        // height 0 - genesis
+        let b_0 = insert_header(&backend, 0, Default::default(), None, Default::default());
+
+        // height 1
+        let b_0_1 = insert_header(&backend, 1, b_0, None, Default::default());
+        let b_0_2 = insert_header(&backend, 1, b_0, None, [1; 32].into());
+        let b_0_3 = insert_header(&backend, 1, b_0, None, [2; 32].into());
+
+        assert_eq!(
+            vec![b_0_1, b_0_2, b_0_3],
+            backend.blockchain().leaves().unwrap()
+        );
+
+        // height 2
+        let b_0_1_1 = insert_header(&backend, 2, b_0_1, None, Default::default());
+        let b_0_2_1 = insert_header(&backend, 2, b_0_2, None, Default::default());
+        let b_0_2_2 = insert_header(&backend, 2, b_0_2, None, [1; 32].into());
+
+        assert_eq!(
+            vec![b_0_1_1, b_0_2_1, b_0_2_2, b_0_3],
+            backend.blockchain().leaves().unwrap()
+        );
+
+        backend.finalize_block(BlockId::Hash(b_0_1), None).unwrap();
+
+        backend
+            .finalize_block(BlockId::Hash(b_0_1_1), None)
+            .unwrap();
+
+        // leaves at the same height stay, leaves at lower height get pruned (`b_0_3`)
+        assert_eq!(
+            vec![b_0_1_1, b_0_2_1, b_0_2_2],
+            backend.blockchain().leaves().unwrap()
+        );
     }
 }
