0ea867be0858992b0a900b03250703493b36205f
adoerr <0xad@gmx.net>
2021-07-25 18:33:33 +0200

    test import justification

--- emptor/src/client.rs
+++ emptor/src/client.rs
@@ -11,6 +11,7 @@ mod tests {
     use super::*;
 
     use sp_consensus::block_import::BlockOrigin;
+    use sp_runtime::{ConsensusEngineId, Justification, Justifications};
 
     use sc_block_builder::BlockBuilderProvider;
     use sc_client_api::HeaderBackend;
@@ -54,6 +55,33 @@ mod tests {
             .block;
 
         executor::block_on(client.import_as_final(BlockOrigin::File, block)).unwrap();
+
+        let info = client.info();
+
+        assert_eq!(1, info.best_number);
+        assert_eq!(1, info.finalized_number);
+    }
+
+    #[test]
+    fn import_justification() {
+        sp_tracing::try_init_simple();
+
+        const ENGINE_ID: ConsensusEngineId = *b"SMPL";
+
+        let mut client = new();
+
+        let block = client
+            .new_block(Default::default())
+            .unwrap()
+            .build()
+            .unwrap()
+            .block;
+
+        let j: Justification = (ENGINE_ID, vec![1, 2, 3]);
+
+        let j = Justifications::from(j);
+
+        executor::block_on(client.import_justified(BlockOrigin::File, block, j)).unwrap();
 
         let info = client.info();
 
