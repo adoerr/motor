5e4fcbc5829c8fbc46116a459781ba7c1e704d5e
adoerr <0xad@gmx.net>
2021-08-05 07:25:27 +0200

    trait Client

--- vegan/src/lib.rs
+++ vegan/src/lib.rs
@@ -14,10 +14,30 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use sc_client_api::{Backend, BlockchainEvents, Finalizer};
+use sp_api::ProvideRuntimeApi;
+use sp_blockchain::HeaderBackend;
+use sp_runtime::traits::Block;
+
+pub trait Client<B, BE>:
+    BlockchainEvents<B> + HeaderBackend<B> + Finalizer<B, BE> + ProvideRuntimeApi<B> + Send + Sync
+where
+    B: Block,
+    BE: Backend<B>,
+{
+    // empty
+}
+
+impl<B, BE, T> Client<B, BE> for T
+where
+    B: Block,
+    BE: Backend<B>,
+    T: BlockchainEvents<B>
+        + HeaderBackend<B>
+        + Finalizer<B, BE>
+        + ProvideRuntimeApi<B>
+        + Send
+        + Sync,
+{
+    // empty
 }
