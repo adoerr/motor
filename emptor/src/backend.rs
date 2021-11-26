17d599f9c016dfdfa29769b26965fbec6b74663c
adoerr <0xad@gmx.net>
2021-11-26 06:14:33 +0100

    remove ChangesTrie stuff

--- emptor/src/backend.rs
+++ emptor/src/backend.rs
@@ -19,33 +19,17 @@
 use sc_client_api::{Backend, BlockImportOperation, NewBlockState};
 use sp_core::H256;
 use sp_runtime::{
-    generic::{BlockId, DigestItem},
+    generic::BlockId,
     testing::{Digest, ExtrinsicWrapper, Header},
     traits::{BlakeTwo256, Hash},
 };
-use sp_state_machine::{ChangesTrieCacheAction, IndexOperation, MemoryDB, TrieDBMut, TrieMut};
+use sp_state_machine::IndexOperation;
 
 #[cfg(test)]
 #[path = "backend_tests.rs"]
 mod tests;
 
 type Block = sp_runtime::testing::Block<ExtrinsicWrapper<u64>>;
-
-pub fn changes_trie(changes: Vec<(Vec<u8>, Vec<u8>)>) -> (H256, MemoryDB<BlakeTwo256>) {
-    let mut root = H256::default();
-    let mut trie_update = MemoryDB::<BlakeTwo256>::default();
-
-    {
-        let mut trie = TrieDBMut::<BlakeTwo256>::new(&mut trie_update, &mut root);
-
-        for (key, val) in changes {
-            trie.insert(&key, &val)
-                .expect("trie k/v pair insert failed");
-        }
-    }
-
-    (root, trie_update)
-}
 
 pub fn insert_header(
     backend: &sc_client_db::Backend<Block>,
@@ -69,20 +53,12 @@ pub fn insert_block(
     backend: &sc_client_db::Backend<Block>,
     number: u64,
     parent_hash: H256,
-    changes: Option<Vec<(Vec<u8>, Vec<u8>)>>,
+    _changes: Option<Vec<(Vec<u8>, Vec<u8>)>>,
     extrinsics_root: H256,
     body: Vec<ExtrinsicWrapper<u64>>,
     transaction_idx: Option<Vec<IndexOperation>>,
 ) -> H256 {
-    let mut digest = Digest::default();
-    let mut changes_trie_update = Default::default();
-
-    if let Some(changes) = changes {
-        let (root, update) = changes_trie(changes);
-
-        digest.push(DigestItem::ChangesTrieRoot(root));
-        changes_trie_update = update;
-    }
+    let digest = Digest::default();
 
     let header = Header {
         parent_hash,
@@ -115,9 +91,6 @@ pub fn insert_block(
         op.update_transaction_index(idx)
             .expect("add transaction idx failed");
     }
-
-    op.update_changes_trie((changes_trie_update, ChangesTrieCacheAction::Clear))
-        .expect("update trie data failed");
 
     backend.commit_operation(op).expect("block insert failed");
 
